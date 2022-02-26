550722fdfd693182cbaa1a453754a55b0b78d921
Christbowel <96427513+Christbowel@users.noreply.github.com>
2022-02-26 12:42:22 +0100

    Add files via upload

---
+++ cbzip.py
@@ -0,0 +1,48 @@
+import zipfile
+from pyfiglet import Figlet
+import datetime
+
+
+a= Figlet(font="banner")
+print (a.renderText("christ bowel"))
+                                                 
+
+def crack_password(password_list, obj):
+    
+    idx = 0
+                                                                                                                                           
+ 
+    with open(password_list, 'rb') as file:
+        for line in file:
+            for word in line.split():
+                try:
+                    idx += 1
+                    obj.extractall(pwd=word)
+                    print("mot de passe trouve' ligne ",idx)
+                    print("le mot de passe trouve est : ", word.decode())
+                    return True
+                except:
+                    continue
+    return False
+
+password_list = input("entrer le nom de la wordlist si vous n'avez pas  entrer passwords.txt: ")
+  
+zip_file = input("entrer le nom du fichier zip ainsi que son extension: ")
+
+
+# initialisation
+
+obj = zipfile.ZipFile(zip_file)
+  
+# calcul du nombre de mot present dans la wordlist
+
+cnt = len(list(open(password_list, "rb")))
+  
+print("il y'a au total ",cnt ,"mots a tester")
+  
+if crack_password(password_list, obj) == False:
+    print("mot de passe introuvable dans ce fichier")
+
+print(datetime.datetime.now())
+
+print ("by christ bowel")
